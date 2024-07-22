6ed18579fb928835dcdce505d079162ead4f8a22
sneha8898 <98009763+sneha8898@users.noreply.github.com>
2024-07-22 22:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-# Imporiting Necessary Libraries
+# Importing Necessary Libraries
 import streamlit as st
 from PIL import Image
 import io
@@ -7,41 +6,35 @@ import numpy as np
 import tensorflow as tf
 from utils import clean_image, get_prediction, make_results
 
-# Loading the Model and saving to cache
+# Define a function to load and cache the model
 @st.cache(allow_output_mutation=True)
 def load_model(path):
+    # Load Xception and DenseNet models from Keras applications
+    xception_base = tf.keras.applications.Xception(include_top=False, weights='imagenet', input_shape=(512, 512, 3))
+    densenet_base = tf.keras.applications.DenseNet121(include_top=False, weights='imagenet', input_shape=(512, 512, 3))
     
-    # Xception Model
-    xception_model = tf.keras.models.Sequential([
-    tf.keras.applications.xception.Xception(include_top=False, weights='imagenet', input_shape=(512, 512, 3)),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(4,activation='softmax')
-    ])
-
-
-    # DenseNet Model
-    densenet_model = tf.keras.models.Sequential([
-        tf.keras.applications.densenet.DenseNet121(include_top=False, weights='imagenet',input_shape=(512, 512, 3)),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(4,activation='softmax')
-    ])
-
-    # Ensembling the Models
+    # Add custom top layers for each model
+    xception_out = tf.keras.layers.GlobalAveragePooling2D()(xception_base.output)
+    xception_out = tf.keras.layers.Dense(4, activation='softmax')(xception_out)
+    
+    densenet_out = tf.keras.layers.GlobalAveragePooling2D()(densenet_base.output)
+    densenet_out = tf.keras.layers.Dense(4, activation='softmax')(densenet_out)
+    
+    # Create the ensemble model
     inputs = tf.keras.Input(shape=(512, 512, 3))
-
+    xception_model = tf.keras.Model(inputs, xception_out)
+    densenet_model = tf.keras.Model(inputs, densenet_out)
+    
     xception_output = xception_model(inputs)
     densenet_output = densenet_model(inputs)
 
-    outputs = tf.keras.layers.average([densenet_output, xception_output])
-
-
+    outputs = tf.keras.layers.Average()([xception_output, densenet_output])
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-    # Loading the Weights of Model
+    # Load model weights
     model.load_weights(path)
-    
-    return model
 
+    return model
 
 # Removing Menu
 hide_streamlit_style = """
@@ -56,44 +49,39 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 model = load_model('model.h5')
 
 # Title and Description
-st.title('Plant Diesease Detection')
+st.title('Plant Disease Detection')
 st.write("Just Upload your Plant's Leaf Image and get predictions if the plant is healthy or not")
 
 # Setting the files that can be uploaded
-uploaded_file = st.file_uploader("Choose a Image file", type=["png", "jpg"])
+uploaded_file = st.file_uploader("Choose an Image file", type=["png", "jpg", "jpeg"])
 
-# If there is a uploaded file, start making prediction
-if uploaded_file != None:
-    
+# If there is an uploaded file, start making a prediction
+if uploaded_file is not None:
     # Display progress and text
     progress = st.text("Crunching Image")
     my_bar = st.progress(0)
-    i = 0
     
     # Reading the uploaded image
     image = Image.open(io.BytesIO(uploaded_file.read()))
-    st.image(np.array(Image.fromarray(
-        np.array(image)).resize((700, 400), Image.ANTIALIAS)), width=None)
-    my_bar.progress(i + 40)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    my_bar.progress(30)
     
     # Cleaning the image
     image = clean_image(image)
+    my_bar.progress(60)
     
     # Making the predictions
     predictions, predictions_arr = get_prediction(model, image)
-    my_bar.progress(i + 30)
+    my_bar.progress(90)
     
     # Making the results
     result = make_results(predictions, predictions_arr)
     
-    # Removing progress bar and text after prediction done
-    my_bar.progress(i + 30)
-    progress.empty()
-    i = 0
-    my_bar.empty()
-    
     # Show the results
     st.write(f"The plant {result['status']} with {result['prediction']} prediction.")
-
     
-        
+    # Final progress
+    my_bar.progress(100)
+    st.success("Prediction complete!")
+else:
+    st.warning("Please upload an image file to get predictions.")
